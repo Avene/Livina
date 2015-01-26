133c5b9ad160a96531ed9a809dc27b1e63e0a8a2
Avene <juc-y@live.jp>
2015-01-26 23:19:51 +0900

    - livina-1 add retrolambda library

--- app/src/main/java/com/avene/avene/livina/fragment/ServersFragment.java
+++ app/src/main/java/com/avene/avene/livina/fragment/ServersFragment.java
@@ -199,16 +199,12 @@ public class ServersFragment extends LivinaFragment
         @Override
         public void remoteDeviceDiscoveryFailed(Registry registry, final RemoteDevice device,
                                                 final Exception ex) {
-            getActivity().runOnUiThread(new Runnable() {
-                public void run() {
-                    Toast.makeText(getActivity(),
-                            "Discovery failed of '" + device.getDisplayString() + "': "
-                                    + (ex != null ? ex.toString() : "Couldn't retrieve " +
-                                    "device/service descriptors"),
-                            Toast.LENGTH_LONG
-                    ).show();
-                }
-            });
+            getActivity().runOnUiThread(() -> Toast.makeText(getActivity(),
+                    "Discovery failed of '" + device.getDisplayString() + "': "
+                            + (ex != null ? ex.toString() : "Couldn't retrieve " +
+                            "device/service descriptors"),
+                    Toast.LENGTH_LONG
+            ).show());
             deviceRemoved(device);
         } // End of optimization, you can remove the whole block if your Android handset is fast
         // (>=600 Mhz)
@@ -238,19 +234,17 @@ public class ServersFragment extends LivinaFragment
         }
 
         public void deviceAdded(final Device device) {
-            getActivity().runOnUiThread(new Runnable() {
-                public void run() {
-                    DeviceDisplay d = new DeviceDisplay(device);
-                    int position = ServersContent.ITEMS.indexOf(d);
-                    if (position >= 0) {
-                        // Device already in the list, re-set new value at same position
-                        ServersContent.removeItem(d);
-                        ServersContent.addItem(position, d);
-                    } else {
-                        ServersContent.addItem(d);
-                    }
-                    mAdapter.notifyDataSetChanged();
+            getActivity().runOnUiThread(() -> {
+                DeviceDisplay d = new DeviceDisplay(device);
+                int position = ServersContent.ITEMS.indexOf(d);
+                if (position >= 0) {
+                    // Device already in the list, re-set new value at same position
+                    ServersContent.removeItem(d);
+                    ServersContent.addItem(position, d);
+                } else {
+                    ServersContent.addItem(d);
                 }
+                mAdapter.notifyDataSetChanged();
             });
         }
 
